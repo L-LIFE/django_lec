8119adb90d080d06330672feb4df017112abcbc4
L-LIFE <seodbsk78@gmail.com>
2024-01-23 21:16:49 +0900

    week 4 django start, resolve login errors

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,32 @@
+"""
+URL configuration for my_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+-로그인 오류 발생시 해결 방안
+    django의 버전으로 인해 5.0.1인 경우 로그인이 안될 수 있다.
+    버전을 확인하고 업데이트를 하여 해결할 수 있다.
+    pip install --upgrade django==4.2.5 업데이트 코드
+"""
+
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'),
-         name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
 ]
